3a1d9e2a34f529af9548a13dbccda79ef9c94903
xhilgerz <alekzander.brysch@my.utsa.edu>
2025-04-10 21:34:58 -0500

    Added basic setter/getters and add/remove to classes

--- app/src/main/java/edu/utsa/threadly/module/Outfit.java
+++ app/src/main/java/edu/utsa/threadly/module/Outfit.java
@@ -1,4 +1,49 @@
 package edu.utsa.threadly.module;
 
+import java.util.ArrayList;
+
 public class Outfit {
+
+    private int id;
+    private String name;
+
+    private ArrayList<Garment> garments;
+
+    Outfit(int id, String name){
+        this.id = id;
+        this.name = name;
+        this.garments = new ArrayList<Garment>();
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Garment> getGarments() {
+        return garments;
+    }
+
+    public void setGarments(ArrayList<Garment> garments) {
+        this.garments = garments;
+    }
+
+    public void addGarment(Garment garment){
+        this.garments.add(garment);
+    }
+
+    public Garment removeGarment(int index){
+        return this.garments.remove(index);
+    }
+
 }
