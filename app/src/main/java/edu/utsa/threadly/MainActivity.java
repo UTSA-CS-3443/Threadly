e6ababf755c920b819a9ac994a7503f9956ab2f7
Robby <robby.f6@icloud.com>
2025-04-16 10:50:56 -0500

    Refactor button functionality and update layout identifiers for navigation

--- app/src/main/java/edu/utsa/threadly/MainActivity.java
+++ app/src/main/java/edu/utsa/threadly/MainActivity.java
@@ -1,12 +1,16 @@
 package edu.utsa.threadly;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import edu.utsa.threadly.Outfit.OutfitsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        Button nextButton = findViewById(R.id.next_button);
+        nextButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, OutfitsActivity.class);
+            startActivity(intent);
         });
     }
 }
